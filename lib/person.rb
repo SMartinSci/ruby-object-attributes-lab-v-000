2fd9862f5659d85c23dbcbcf4bc96997b2995934
Sherry Martin <sm674@cornell.edu>
2018-09-07 17:56:17 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,11 +7,11 @@ class Person
     @name = new_name
   end
   
-  def job=(new_job)
-    @job = new_job
-  end
-  
   def job 
     @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
   end
 end
